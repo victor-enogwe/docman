deb690719dde4eca98e3df8a37c557ff7e06f524
andela-venogwe <victor.enogwe@andela.com>
2017-02-23 05:02:48 +0100

    export the api routs

---
+++ server/controllers/routes/Routes.js
@@ -0,0 +1,13 @@
+import home from './home';
+import users from './users';
+import documents from './documents';
+import search from './search';
+
+const Routes = {
+  home,
+  users,
+  documents,
+  search
+};
+
+export default Routes;
