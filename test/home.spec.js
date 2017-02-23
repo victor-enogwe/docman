85867c23b1e33f2a5a6788c8b8fcf1bd5e16fd09
andela-venogwe <victor.enogwe@andela.com>
2017-02-23 05:03:34 +0100

    add home page api test

--- test/home.spec.js
+++ test/home.spec.js
@@ -5,10 +5,11 @@ const should = helper.should;
 const app = supertest(helper.app);
 
 describe('Home:', () => {
-  it('Should return http code 201 if a Regular User is created', (done) => {
+  it('Should return http code 200 and a welcome message', (done) => {
     app.get('/')
     .end((error, response) => {
       response.status.should.equal(200);
+      response.body.message.should.equal('Welcome to the document manage api.');
       done();
     });
   });
