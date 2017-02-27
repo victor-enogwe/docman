1b574b6ec1c8892cf54750e32040fb97ef42d35f
andela-venogwe <victor.enogwe@andela.com>
2017-02-27 22:20:50 +0100

    add search controller api

--- server/controllers/api/Search.js
+++ server/controllers/api/Search.js
@@ -1,14 +1,46 @@
 import db          from '../../models/';
 import validate    from '../middlewares/validate';
 
-const searchModel = db.Document;
+const searchDocuments = db.Document;
+const searchUsers = db.Users;
 
 export default {
-  search(req, res) {
-    return searchModel.findAndCountAll({
+
+  /**
+   * search all documents available
+   * @param {Object} req the request object
+   * @param {Object} res the response object
+   * @returns {Object} the response body
+   */
+  searchDocuments(req, res) {
+    return searchDocuments.findAndCountAll({
       where: ['MATCH (title, excerpt) AGAINST(?)',
         req.query.phrase ? [req.query.phrase] : ['']],
       attributes: validate.filterDocumentDetails(),
+      offset: req.query.offset,
+      limit: req.query.limit,
+    })
+    .then((results) => {
+      if (results.rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: `no results found for your query ${req.query.phrase}`
+        });
+      }
+      return res.status(200).json({ success: true, results });
+    });
+  },
+  /**
+   * search all users available
+   * @param {Object} req the request object
+   * @param {Object} res the response object
+   * @returns {Object} the response body
+   */
+  searchUsers(req, res) {
+    return searchUsers.findAndCountAll({
+      where: ['MATCH (username, email) AGAINST(?)',
+        req.query.user ? [req.query.user] : ['']],
+      attributes: validate.filterUserDetails(),
       offset: req.query.offset,
       limit: req.query.limit,
     })
