49ab337663ac93b9501c09f182cf7fe845acaa82
andela-venogwe <victor.enogwe@andela.com>
2017-03-01 13:58:26 +0100

    add new search routes

--- server/controllers/routes/search.js
+++ server/controllers/routes/search.js
@@ -6,7 +6,12 @@ const search = express.Router();
 
 search.use(auth.checkToken, auth.setQueryParameters);
 
-search.get('/documents', Search.searchDocuments);
-search.get('/users', auth.isAdmin, Search.searchUsers);
+search.get('/documents', auth.getSearchPhrase, auth.searchQueryAccess,
+Search.searchDocuments);
+
+search.get('/documents/user/:id', auth.getSearchPhrase, auth.searchQueryAccess,
+Search.searchDocuments);
+search.get('/users', auth.isAdmin, auth.searchQueryUsers,
+auth.setQueryParameters, Search.searchUsers);
 
 export default search;
