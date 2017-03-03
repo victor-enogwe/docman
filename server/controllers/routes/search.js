d786a0c6c2a95d70ca29261f5f98d52582d9a738
andela-venogwe <victor.enogwe@andela.com>
2017-03-03 18:12:33 +0100

    add spacing to routes

--- server/controllers/routes/search.js
+++ server/controllers/routes/search.js
@@ -12,6 +12,7 @@ Search.searchDocuments);
 
 search.get('/documents/user/:id', utils.getSearchPhrase,
 utils.searchQueryAccess, Search.searchDocuments);
+
 search.get('/users', auth.isAdmin, utils.searchQueryUsers,
 utils.setQueryParameters, Search.searchUsers);
 
