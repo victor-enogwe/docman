b5774252b35c94b37967653746cd733e0a99adb5
andela-venogwe <victor.enogwe@andela.com>
2017-03-03 18:09:07 +0100

    abstract query to create indexes

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ import logger                from 'morgan';
 import Logger                from 'js-logger';
 import bodyParser            from 'body-parser';
 import http                  from 'http';
-import mysql                 from 'mysql';
+import mysql                 from 'promise-mysql';
 import bcrypt                from 'bcrypt-nodejs';
-import dbConfig             from './config/db.env.config';
+import dbConfig              from './config/db.env.config';
 import db                    from './server/models/';
 import Routes                from './server/controllers/routes/Routes';
+import query                 from './config/query';
 
 dotenv.config();
 debug('docman:app');
@@ -109,47 +110,38 @@ app.use('/api/v1/search', Routes.search);
 server.on('listening', onListening);
 server.on('error', onError);
 
-dbConnection.connect((err) => {
-  if (!err) {
-    dbConnection
-    .query(`CREATE DATABASE IF NOT EXISTS ${database}`, (error) => {
-      dbConnection.end(() => Logger
-      .warn('🚧 Initial Database Connection Successful. App Booting Up ...'));
-      if (!error) {
-        db.sequelize
-        .sync()
-        .then(() => db.User.findOne({ where: {
-          $or: [{
-            username: process.env.ADMIN_USERNAME
-          }, { email: process.env.ADMIN_EMAIL }]
-        } }).then((userExists) => {
-          if (!userExists) {
-            return db.User.create({
-              roleId: 0,
-              username: process.env.ADMIN_USERNAME,
-              firstname: process.env.ADMIN_FIRSTNAME,
-              lastname: process.env.ADMIN_LASTNAME,
-              email: process.env.ADMIN_EMAIL,
-              password: process.env.ADMIN_PASSWORD,
-              password_confirmation: process.env.ADMIN_PASSWORD,
-              password_digest: bcrypt.hashSync(process.env.ADMIN_PASSWORD,
-              bcrypt.genSaltSync(10)),
-              createdAt: Date.now(),
-              updatedAt: Date.now()
-            });
-          }
-        })
-        .then(() => {
-          server.listen(port, () => {
-            Logger.warn(`🚧 App is Listening on ${port}`);
-          });
-        })).catch(err => Logger.error(err));
-      }
-    });
-  } else {
-    Logger.errpr('🚧 App failed to start');
+dbConnection.then(connection => connection
+.query(`CREATE DATABASE IF NOT EXISTS ${database}`))
+.then(() => Logger
+.warn('🚧 Initial Database Connection Successful. App Booting Up ...'))
+.then(() => db.sequelize.sync())
+.then(() => db.User.findOne({ where: {
+  $or: [{
+    username: process.env.ADMIN_USERNAME
+  }, { email: process.env.ADMIN_EMAIL }]
+} }))
+.then((userExists) => {
+  if (!userExists) {
+    return db.User.create({
+      roleId: 0,
+      username: process.env.ADMIN_USERNAME,
+      firstname: process.env.ADMIN_FIRSTNAME,
+      lastname: process.env.ADMIN_LASTNAME,
+      email: process.env.ADMIN_EMAIL,
+      password: process.env.ADMIN_PASSWORD,
+      password_confirmation: process.env.ADMIN_PASSWORD,
+      password_digest: bcrypt.hashSync(process.env.ADMIN_PASSWORD,
+      bcrypt.genSaltSync(10)),
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    }).then(() => db.sequelize.query(query.query, { raw: true }));
   }
-});
+  return 'Admin User Already Created... All Set...';
+})
+.then(() => server.listen(port, () => Logger
+.warn(`🚧 Admin User Already Created... All Set...
+🚧 App is Listening on ${port}`)))
+.catch(err => Logger.error(err));
 
 export default app;
 
