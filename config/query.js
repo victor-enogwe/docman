6ddebbafef1c8c3c8c6d382c8270b1113eb2eafa
andela-venogwe <victor.enogwe@andela.com>
2017-03-08 14:03:08 +0100

    add user document object to query object

--- config/query.js
+++ config/query.js
@@ -1,5 +1,14 @@
+import faker  from 'Faker';
+
 export default {
   query: 'ALTER TABLE Document ADD FULLTEXT INDEX `Document_Index` \
 (`title`, `excerpt`);\
-  ALTER TABLE User ADD FULLTEXT INDEX `User_Index` (`username`, `email`);'
+  ALTER TABLE User ADD FULLTEXT INDEX `User_Index` (`username`, `email`);',
+  userDocument: {
+    creatorId: 1,
+    title: faker.Lorem.sentence(),
+    excerpt: faker.Lorem.sentences(),
+    access: 'private',
+    content: faker.Lorem.paragraph()
+  }
 };
