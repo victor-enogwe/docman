7140cf37cc8d5be813a16a3428fd1f776d6a3dd7
andela-venogwe <victor.enogwe@andela.com>
2017-03-08 17:56:50 +0100

    remove faker from query file

--- config/query.js
+++ config/query.js
@@ -1,14 +1,12 @@
-import faker  from 'Faker';
-
 export default {
   query: 'ALTER TABLE Document ADD FULLTEXT INDEX `Document_Index` \
 (`title`, `excerpt`);\
   ALTER TABLE User ADD FULLTEXT INDEX `User_Index` (`username`, `email`);',
   userDocument: {
     creatorId: 1,
-    title: faker.Lorem.sentence(),
-    excerpt: faker.Lorem.sentences(),
+    title: 'default admin document',
+    excerpt: 'excerpt for default admin document',
     access: 'private',
-    content: faker.Lorem.paragraph()
+    content: 'excerpt for default admin document'
   }
 };
