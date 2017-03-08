d794861d2cc2b5c3d0594c86a06afd8d15eed6a1
andela-venogwe <victor.enogwe@andela.com>
2017-03-08 14:03:27 +0100

    refactor environment variables

--- config/db.env.config.js
+++ config/db.env.config.js
@@ -2,33 +2,12 @@ import dotenv    from 'dotenv';
 
 dotenv.config();
 
-let database, user, password, host, dialect, port;
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    database = process.env.DEV_DB_NAME;
-    user = process.env.DEV_DB_USER;
-    password = process.env.DEV_DB_PASS;
-    host = process.env.DEV_DB_HOST;
-    dialect = process.env.DEV_DB_TYPE;
-    port = process.env.DEV_DB_PORT;
-    break;
-  case 'test':
-    database = process.env.TEST_DB_NAME;
-    user = process.env.TEST_DB_USER;
-    password = process.env.TEST_DB_PASS;
-    host = process.env.TEST_DB_HOST;
-    dialect = process.env.TEST_DB_TYPE;
-    port = process.env.TEST_DB_PORT;
-    break;
-  default:
-    database = process.env.DB_NAME;
-    user = process.env.DB_USER;
-    password = process.env.DB_PASS;
-    host = process.env.DB_HOST;
-    dialect = process.env.DB_TYPE;
-    port = process.env.DB_PORT;
-}
+const database = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const dialect = process.env.DB_TYPE;
+const port = process.env.DB_PORT;
 
 export default {
   database,
