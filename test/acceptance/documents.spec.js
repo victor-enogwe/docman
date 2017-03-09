28673989166ca6036d693e28a44a05363dfa43b5
andela-venogwe <victor.enogwe@andela.com>
2017-03-09 05:29:29 +0100

    remove mysql travis create database

--- test/acceptance/documents.spec.js
+++ test/acceptance/documents.spec.js
@@ -7,25 +7,26 @@ let regularUser, adminToken, regularUserToken, privateDocument, publicDocument;
 
 describe('Document Api', () => {
   before((done) => {
-    app.post('/login').send({
+    db.sequelize.sync()
+    .then(() => app.post('/login').send({
       username: testData.adminUser.username,
       password: testData.adminUser.password
     })
     .then((res) => {
       adminToken = res.body.data.token;
-      app.post('/api/v1/users').send(testData.validUser)
-      .then((response) => {
-        regularUser = response.body.data.user;
-        app.post('/login').send({
-          username: testData.validUser.username,
-          password: testData.validUser.password
-        })
-        .then((res) => {
-          regularUserToken = res.body.data.token;
-          done();
-        });
-      });
-    });
+    })
+    .then(() => app.post('/api/v1/users').send(testData.validUser))
+    .then((response) => {
+      regularUser = response.body.data.user;
+    })
+    .then(() => app.post('/login').send({
+      username: testData.validUser.username,
+      password: testData.validUser.password
+    }))
+    .then((res) => {
+      regularUserToken = res.body.data.token;
+    })
+    .then(() => done()));
   });
 
   after(() => db.User.destroy({ where: { roleId: 1 } })
